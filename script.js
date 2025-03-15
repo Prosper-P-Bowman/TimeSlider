50c18d8e939801b97faab83a94ee55f7f7bae93c
prosper <prosper@kali.hostle>
2025-03-15 11:49:45 -0500

    TimeSlider

--- script.js
+++ script.js
@@ -10,23 +10,23 @@ document.addEventListener("DOMContentLoaded", function() {
       myRange.innerHTML = slider.value;
 
       if(myRange.innerHTML == "100"){
-        document.getElementById("image").style.backgroundImage = "url('/pic/love2.jpg')";
-        document.getElementById("image").style.backgroundSize = "cover";
+        document.getElementById("image").style.backgroundImage = "url('./pic/love2.jpg')";
+        document.getElementById("image").style.backgroundSize = "contain";
         document.getElementById("image").style.backgroundPosition = "center";
         document.getElementById("image").style.backgroundRepeat = "no-repeat";  
       } else if(myRange.innerHTML == "75"){
-        document.getElementById("image").style.backgroundImage = "url('/pic/love3.jpg')";
-        document.getElementById("image").style.backgroundSize = "cover";
+        document.getElementById("image").style.backgroundImage = "url('./pic/love3.jpg')";
+        document.getElementById("image").style.backgroundSize = "contain";
         document.getElementById("image").style.backgroundPosition = "center";
         document.getElementById("image").style.backgroundRepeat = "no-repeat";  
       } else if(myRange.innerHTML == "50"){
-        document.getElementById("image").style.backgroundImage = "url('/pic/love4.jpg')";
-        document.getElementById("image").style.backgroundSize = "cover";
+        document.getElementById("image").style.backgroundImage = "url('./pic/love4.jpg')";
+        document.getElementById("image").style.backgroundSize = "contain";
         document.getElementById("image").style.backgroundPosition = "center";
         document.getElementById("image").style.backgroundRepeat = "no-repeat";  
       } else if(myRange.innerHTML == "25"){
-        document.getElementById("image").style.backgroundImage = "url('/pic/love5.jpg')";
-        document.getElementById("image").style.backgroundSize = "cover";
+        document.getElementById("image").style.backgroundImage = "url('./pic/love5.jpg')";
+        document.getElementById("image").style.backgroundSize = "contain";
         document.getElementById("image").style.backgroundPosition = "center";
         document.getElementById("image").style.backgroundRepeat = "no-repeat";  
       }  else {
